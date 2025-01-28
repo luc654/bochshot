3b1e912620111d1062f4b0995f3c8913b4a8d80f
luc654 <113506883+luc654@users.noreply.github.com>
2025-01-28 23:58:32 +0100

    Create main.py
    Initialize starting code. This code features debug material and should not be used as of currently

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+
+class Player:
+    def __init__(self, lives, name):
+        self.lives = lives
+        self.name = name
+        
+    def removeLive(self):
+        if self.lives > 0:
+            self.lives -= 1
+        else:
+            return "Dead"
+            
+    def getName(self):
+        return self.name
+
+def generateShells():
+    blanks = random.randint(1, 6)
+    live = random.randint(1, 6)
+    total = blanks + live
+    listValue = {"blanks": blanks, "live": live, "total": total}
+    return listValue
+
+def showShells(shellList):
+    blank = "◯"
+    live = "⬤"
+    display_string = ""
+
+    # Add blanks (◯)
+    for _ in range(shellList['blanks']):
+        display_string += blank
+
+    # Add lives (⬤)
+    for _ in range(shellList['live']):
+        display_string += live
+
+    return display_string
+shellList = generateShells()
+
+def updatelist(shell_type):
+    shellList[shell_type] -= 1  
+
+def shoot():
+    total = shellList['blanks'] + shellList['live']  
+    shot = random.randint(1, total)  
+
+    if shot <= shellList['live']:  
+        updatelist('live')
+        return 'live'
+    else:  
+        updatelist('blanks')
+        return 'blank'
+
+
+print(showShells(shellList))
+
+player1 = Player(3, "player 1")
+player2 = Player(3, "player 2")
+
+print(shoot())
+print(showShells(shellList))
+
+# Main game loop
+
